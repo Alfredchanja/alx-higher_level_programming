827c11c8300699b4498ff7f0ce1791c7eb67d01b
Alfred <alfredgachanja8@gmail.com>
2022-09-20 11:30:12 -0700

    Hello print and integers

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/puthon3
+number = 98
+
+#converts the string into regular number integer
+number = int(number)
+
+#prints the number and the statement
+print('{} Bettery street'.format(number))
